950131ff1e85375461cecde0354b3457102b82b2
tooomm <tooomm@users.noreply.github.com>
2021-05-17 00:21:26 +0200

    sizepolicy /3

--- cockatrice/src/dlg_settings.cpp
+++ cockatrice/src/dlg_settings.cpp
@@ -1053,7 +1053,7 @@ ShortcutSettingsPage::ShortcutSettingsPage()
     // table
     shortcutsTable = new QTreeWidget();
     shortcutsTable->setColumnCount(2);
-    shortcutsTable->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    shortcutsTable->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     shortcutsTable->setUniformRowHeights(true);
     shortcutsTable->setAlternatingRowColors(true);
     shortcutsTable->header()->resizeSection(0, shortcutsTable->width() / 3 * 2);
